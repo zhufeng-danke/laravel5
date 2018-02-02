8dd89549b0ffe76d7be41509401f5494caa3aa60
zhufeng <zhufeng@dankegongyu.com>
2018-02-02 11:32:09 +0800

    validation rule

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+include_once('demos/the-basics/validation.php');
 
 Route::get('/', function () {
     return view('welcome');
